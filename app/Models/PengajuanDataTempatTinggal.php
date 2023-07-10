4ff0618d6e40971d7b85910b017e81335e51d596
johansensagala <94831383+johansensagala@users.noreply.github.com>
2023-07-10 11:34:01 +0700

    Update PengajuanDataTempatTinggal.php
    Signed-off-by: johansensagala <94831383+johansensagala@users.noreply.github.com>

--- app/Models/PengajuanDataTempatTinggal.php
+++ app/Models/PengajuanDataTempatTinggal.php
@@ -9,8 +9,12 @@ class PengajuanDataTempatTinggal extends Model
 {
     use HasFactory;
 
-    protected $guarded = ['id'];
-
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
+    
     public function penjamin ()
     {
         return $this->belongsTo(Penjamin::class);
